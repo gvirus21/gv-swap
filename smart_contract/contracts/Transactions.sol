315517f69ef1b9d40b0a1c86cb481c2e3e9b726c
gourav <gvirus21yt@gmail.com>
2022-04-07 16:48:09 +0530

    - Written Transactions.sol smart contract - implemented deploy script - modified hardhat.cofig.js file for deployment

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+contract Transactions {
+
+    event Transfer(
+        address sender, 
+        address reciever, 
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+        );
+
+    function publishTransaction(address payable receiver, uint256 amount, string memory message, string memory keyword) public {
+        emit Transfer(msg.sender , receiver, amount, message, block.timestamp, keyword);
+    }
+
+
+}
